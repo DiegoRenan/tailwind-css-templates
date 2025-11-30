b283b5fa18baaedd2a634031f400d10c1f80fef4
Renan <renan.ccomp@gmail.com>
2025-11-30 17:05:56 -0600

    container

---
+++ layout/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <div className="container bg-red-300 mx-auto p-4 my-4 text-red-900 border-red-600 border-2">
+            <p>O texto do container</p>
+        </div>
+    </>
+  )
+}
+
+export default App
