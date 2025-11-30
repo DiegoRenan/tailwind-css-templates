80f8ab862ef9f2e8f0903bb3f40d71962c7c8ed8
Renan <renan.ccomp@gmail.com>
2025-11-30 17:52:14 -0600

    container with breaking point

--- layout/src/App.jsx
+++ layout/src/App.jsx
@@ -8,9 +8,13 @@ function App() {
 
   return (
     <>
-        <div className="container bg-red-300 mx-auto p-4 my-4 text-red-900 border-red-600 border-2">
-            <p>O texto do container</p>
-        </div>
+      <div className="container bg-red-300 mx-auto p-4 my-4 text-red-900 border-red-600 border-2">
+          <p>O texto do container</p>
+      </div>
+
+      <div className="container bg-red-300 md:bg-green-800 mx-auto p-4 my-4 text-red-900 border-red-600 border-2">
+          <p>container com breaking point</p>
+      </div>
     </>
   )
 }
