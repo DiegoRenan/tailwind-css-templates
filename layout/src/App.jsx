2dc58b098084a8f08c0850ebec871d5f73ad0a2a
Renan <renan.ccomp@gmail.com>
2025-12-02 10:07:09 -0600

    overflow

--- layout/src/App.jsx
+++ layout/src/App.jsx
@@ -10,11 +10,11 @@ function App() {
     <>
       <h1>Container</h1><hr />
       <div className="container bg-red-300 mx-auto p-4 my-4 text-red-900 border-red-600 border-2">
-          <p>O texto do container</p>
+        <p>O texto do container</p>
       </div>
 
       <div className="container bg-red-300 md:bg-green-800 mx-auto p-4 my-4 text-red-900 border-red-600 border-2">
-          <p>container com breaking point</p>
+        <p>container com breaking point</p>
       </div>
 
       <h2>Box Model</h2><hr />
@@ -28,11 +28,11 @@ function App() {
 
       <h2>Display in-line vs Display block</h2><hr />
       <div className="container mx-auto py-1">
-          <span className="block bg-red-300 border-2">Span in block</span>
-          <span className="block bg-red-300 border-2">Span in block 2</span>
-          <div className="inline-block border-2 bg-blue-500">Div in-line</div>
-          <div className="inline-block border-2 bg-blue-500">Div in-line 2</div>
-          <div className="border-2 bg-blue-500 hidden"></div>
+        <span className="block bg-red-300 border-2">Span in block</span>
+        <span className="block bg-red-300 border-2">Span in block 2</span>
+        <div className="inline-block border-2 bg-blue-500">Div in-line</div>
+        <div className="inline-block border-2 bg-blue-500">Div in-line 2</div>
+        <div className="border-2 bg-blue-500 hidden"></div>
       </div>
 
       <h2>Float</h2><hr />
@@ -41,7 +41,23 @@ function App() {
       <div className="clear-both"></div>
       <div className="bg-gray-500 float-right">Float Right</div>
       <div className="bg-gray-700 float-left">Float Left</div>
-      
+
+      <h2>Overflow</h2>
+      <hr />
+
+      <div className="w-40 h-40 bg-yellow-300 overflow-auto">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+
+      <div className="w-40 h-40 overflow-y-auto bg-yellow-200">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+
+      <div className="w-100 h-40 overflow-auto bg-yellow-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
     </>
   )
 }
