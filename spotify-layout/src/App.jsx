ce320414052fc0718d6a412038ad137249f21f5a
Renan <renan.ccomp@gmail.com>
2025-12-04 16:14:03 -0600

    spotify clone

---
+++ spotify-layout/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './assets/css/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="bg-red-900">Teste</h1>
+      <i className="fab fa-instagram"></i>
+    </>
+  )
+}
+
+export default App
