b50a61092f752e8b5f986fc1514d13edd5e320f3
Renan <renan.ccomp@gmail.com>
2025-12-11 16:18:08 -0600

    nav

--- spotify-layout/src/App.jsx
+++ spotify-layout/src/App.jsx
@@ -2,14 +2,34 @@ import { useState } from 'react'
 import './assets/css/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1 className="bg-red-900">Teste</h1>
-      <i className="fab fa-instagram"></i>
-    </>
-  )
+    return (
+        <>
+            <header>
+                <div className="flex fixed bg-black w-full px-3 md:px-20 py-4">
+                    <div className="container flex items-center mx-auto">
+                    <div><a href="#"><img src="/src/assets/images/spotify-logo.png" alt="Spotify" className="w-20 md:w-spotify-125px" /></a></div>
+                    <div className="flex justify-end flex-1 text-white text-3xl md:hidden">
+                        <i className="fas fa-bars bg-red-700"></i>
+                    </div>
+                    <div className="items-end flex-1 text-white font-bold hidden md:flex">
+                        <nav className="flex-1">
+                            <ul className="flex justify-end flex-1">
+                                <li className="px-4"><a className="hover:text-spotify-hover" href="#">Premium</a></li>
+                                <li className="px-4"><a className="hover:text-spotify-hover" href="#">Ajuda</a></li>
+                                <li className="px-4"><a className="hover:text-spotify-hover" href="#">Baixar</a></li>
+                                <li className="px-4"><span className="border-r border-white"></span></li>
+                                <li className="px-4"><a className="hover:text-spotify-hover" href="#">Inscrever-se</a></li>
+                                <li className="px-4"><a className="hover:text-spotify-hover" href="#">Log-in</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            </header>
+        </>
+    )
 }
 
 export default App
